0db6bd692619b903b734491aae42d4d7e773c4eb
Naitri_Rajyaguru <naitrir21@gmail.com>
2023-09-27 14:37:51 -0400

    Updated KITTI_dataset.py

--- dataset/KITTI_dataset.py
+++ dataset/KITTI_dataset.py
@@ -4,10 +4,10 @@ import os
 import numpy as np
 from PIL import Image
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import scipy.io as sio
 from path import Path
-from utils.utils import rotationError, read_pose_from_text
+from utils.utils import read_pose_from_text  # Assuming this function reads the 3x4 extrinsic matrix
 from utils import custom_transform
 from collections import Counter
 from scipy.ndimage import gaussian_filter1d
@@ -19,59 +19,40 @@ IMU_FREQ = 10
 class KITTI(Dataset):
     def __init__(self, root,
                  sequence_length=11,
-                 train_seqs=['00', '01', '02', '04', '06', '08', '09'],
+                 train_seqs=['00', '01', '02', '04', '05','06'],
                  transform=None):
-        
         self.root = Path(root)
         self.sequence_length = sequence_length
         self.transform = transform
         self.train_seqs = train_seqs
         self.make_dataset()
-    
+
     def make_dataset(self):
         sequence_set = []
         for folder in self.train_seqs:
-            poses, poses_rel = read_pose_from_text(self.root/'poses/{}.txt'.format(folder))
+            gts = np.loadtxt(self.root/'poses/{}.txt'.format(folder))  # Load 3x4 extrinsic matrix
             imus = sio.loadmat(self.root/'imus/{}.mat'.format(folder))['imu_data_interp']
+            
             fpaths = sorted((self.root/'sequences/{}/image_2'.format(folder)).files("*.png"))      
             for i in range(len(fpaths)-self.sequence_length):
                 img_samples = fpaths[i:i+self.sequence_length]
                 imu_samples = imus[i*IMU_FREQ:(i+self.sequence_length-1)*IMU_FREQ+1]
-                pose_samples = poses[i:i+self.sequence_length]
-                pose_rel_samples = poses_rel[i:i+self.sequence_length-1]
-                segment_rot = rotationError(pose_samples[0], pose_samples[-1])
-                sample = {'imgs':img_samples, 'imus':imu_samples, 'gts': pose_rel_samples, 'rot': segment_rot}
+                extrinsic_samples = gts.reshape(3,4)
+                sample = {'imgs': img_samples, 'imus': imu_samples, 'gts': extrinsic_samples}
                 sequence_set.append(sample)
         self.samples = sequence_set
-        
-        # Generate weights based on the rotation of the training segments
-        # Weights are calculated based on the histogram of rotations according to the method in https://github.com/YyzHarry/imbalanced-regression
-        rot_list = np.array([np.cbrt(item['rot']*180/np.pi) for item in self.samples])
-        rot_range = np.linspace(np.min(rot_list), np.max(rot_list), num=10)
-        indexes = np.digitize(rot_list, rot_range, right=False)
-        num_samples_of_bins = dict(Counter(indexes))
-        emp_label_dist = [num_samples_of_bins.get(i, 0) for i in range(1, len(rot_range)+1)]
-
-        # Apply 1d convolution to get the smoothed effective label distribution
-        lds_kernel_window = get_lds_kernel_window(kernel='gaussian', ks=7, sigma=5)
-        eff_label_dist = convolve1d(np.array(emp_label_dist), weights=lds_kernel_window, mode='constant')
-
-        self.weights = [np.float32(1/eff_label_dist[bin_idx-1]) for bin_idx in indexes]
 
     def __getitem__(self, index):
         sample = self.samples[index]
         imgs = [np.asarray(Image.open(img)) for img in sample['imgs']]
         
         if self.transform is not None:
-            imgs, imus, gts = self.transform(imgs, np.copy(sample['imus']), np.copy(sample['gts']))
+            imgs, imus, gts = self.transform(imgs, np.copy(sample['imus']), sample['gts'])
         else:
             imus = np.copy(sample['imus'])
-            gts = np.copy(sample['gts']).astype(np.float32)
+            gts = sample['gts']
         
-        rot = sample['rot'].astype(np.float32)
-        weight = self.weights[index]
-
-        return imgs, imus, gts, rot, weight
+        return imgs, imus, gts
 
     def __len__(self):
         return len(self.samples)
@@ -88,20 +69,6 @@ class KITTI(Dataset):
 
         return fmt_str
 
-
-def get_lds_kernel_window(kernel, ks, sigma):
-    assert kernel in ['gaussian', 'triang', 'laplace']
-    half_ks = (ks - 1) // 2
-    if kernel == 'gaussian':
-        base_kernel = [0.] * half_ks + [1.] + [0.] * half_ks
-        kernel_window = gaussian_filter1d(base_kernel, sigma=sigma) / max(gaussian_filter1d(base_kernel, sigma=sigma))
-    elif kernel == 'triang':
-        kernel_window = triang(ks)
-    else:
-        laplace = lambda x: np.exp(-abs(x) / sigma) / (2. * sigma)
-        kernel_window = list(map(laplace, np.arange(-half_ks, half_ks + 1))) / max(map(laplace, np.arange(-half_ks, half_ks + 1)))
-
-    return kernel_window
-
-
-
+# Create a DataLoader to load batches of the KITTI dataset
+dataset = KITTI(root='/mnt/ddb5152f-7808-4a33-9f27-ba0a0d7f3164/Naitri/AutoCalib/visual-selective-vio/data')
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
